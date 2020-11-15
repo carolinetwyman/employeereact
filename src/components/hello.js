4b2c89e5a1da123da9374264b8d47a01d0981be7
Caroline Twyman <carolinetwyman@gmail.com>
2020-11-15 12:29:13 -0600

    adding markup

---
+++ src/components/hello.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function TestDiv() {
+  return (
+    <div>
+      <h1>Header!</h1>
+      <h2>Header too!</h2>
+      <ul>
+        <li>long island</li>
+        <li>long island</li>
+        <li>long island</li>
+      </ul>
+    </div>
+  );
+}
+
+export default TestDiv;
